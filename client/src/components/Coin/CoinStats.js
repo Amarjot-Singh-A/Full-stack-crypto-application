1ce74a56ed39c4eddde49ecd654c059974fcbf0d
amarjot <amarjotsingh90@outlook.com>
2021-10-02 15:29:12 -0700

    improve table structure in coin.js

--- client/src/components/Coin/CoinStats.js
+++ client/src/components/Coin/CoinStats.js
@@ -8,10 +8,25 @@ import TableRow from "@mui/material/TableRow";
 import TableBody from "@mui/material/TableBody";
 import Title from "../Title/Title";
 
-/**
- * TODO - change the table structure to remove repitition
- */
+
 export default function CoinStats({ stats }) {
+  let tableData =
+    Object.keys(stats).length !== 0
+      ? {
+          Image: stats.image.small,
+          Name: stats.name,
+          Symbol: stats.symbol,
+          "All time High": stats.market_data.ath.cad,
+          "All time Low": stats.market_data.atl.cad,
+          "Market Cap": stats.market_data.market_cap.cad,
+          "Total Volume": stats.market_data.total_volume.cad,
+          "High 24h": stats.market_data.high_24h.cad,
+          "Low 24h": stats.market_data.low_24h.cad,
+          "Circulatory Supply ": stats.market_data.circulating_supply,
+          "Max Supply": stats.market_data.max_supply,
+        }
+      : {};
+
   return (
     Object.keys(stats).length !== 0 && (
       <TableContainer component={Paper}>
@@ -24,62 +39,25 @@ export default function CoinStats({ stats }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-              <TableCell align="left">Image</TableCell>
-              <TableCell align="left">
-                <img src={stats.image.small} alt="coinimage" />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="left">{stats.name}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Symbol</TableCell>
-              <TableCell align="left">{stats.symbol}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">All Time High</TableCell>
-              <TableCell align="left">{stats.market_data.ath.cad}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">All Time Low</TableCell>
-              <TableCell align="left">{stats.market_data.atl.cad}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Market Cap</TableCell>
-              <TableCell align="left">
-                {stats.market_data.market_cap.cad}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Total Volume</TableCell>
-              <TableCell align="left">
-                {stats.market_data.total_volume.cad}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">High 24h</TableCell>
-              <TableCell align="left">
-                {stats.market_data.high_24h.cad}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Low 24h</TableCell>
-              <TableCell align="left">
-                {stats.market_data.low_24h.cad}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Circulating Supply</TableCell>
-              <TableCell align="left">
-                {stats.market_data.circulating_supply}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell align="left">Max Supply</TableCell>
-              <TableCell align="left">{stats.market_data.max_supply}</TableCell>
-            </TableRow>
+            {Object.entries(tableData).map(([key, value]) => {
+              if (key === "Image") {
+                return (
+                  <TableRow key={key}>
+                    <TableCell align="left">{key}</TableCell>
+                    <TableCell align="left">
+                      <img src={value} alt={key} />
+                    </TableCell>
+                  </TableRow>
+                );
+              } else {
+                return (
+                  <TableRow key={key}>
+                    <TableCell align="left">{key}</TableCell>
+                    <TableCell align="left">{value}</TableCell>
+                  </TableRow>
+                );
+              }
+            })}
           </TableBody>
         </Table>
       </TableContainer>
